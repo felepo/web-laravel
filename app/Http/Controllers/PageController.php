55199e00ab705258a303851364972f3b8a4d3f07
Fernando Poncio <fecrackle@gmail.com>
2025-07-22 22:08:36 -0600

    Implement PageController and home view for displaying questions. A basic view

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Question;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        $questions = Question::with('category', 'user')->latest()->get();
+
+        return view('pages.home', [
+            'questions' => $questions,
+        ]);
+    }
+}
