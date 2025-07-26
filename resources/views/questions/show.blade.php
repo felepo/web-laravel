a7ed5fee4e9545d437af3665924f6c1f21c78908
Fernando Poncio <fecrackle@gmail.com>
2025-07-26 00:24:27 -0600

    Add Comment Livewire component and integrate into question and answer views

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -36,31 +36,8 @@
             {{ $question->description }}
         </p>
 
-        <ul class="my-4 space-y-2">
+        <livewire:comment :commentable="$question" />
 
-            @foreach ($question->comments as $comment)
-            <li class="flex items-center gap-2">
-                <p class="text-xs bg-white/10 p-4 rounded-md">
-                    <span class="text-gray-500">
-                        {{ $comment->user->name }} |
-                        {{ $comment->created_at->diffForHumans() }}
-                    </span>
-                    <span class="text-gray-300">
-                        {{ $comment->content }}
-                    </span>
-                </p>
-
-                <div>&hearts;</div>
-            </li>
-            @endforeach
-
-        </ul>
-
-        <p class="text-gray-500">
-            <a href="#" class="rounded-md text-xs hover:underline cursor-pointer">
-                Agregar comentario
-            </a>
-        </p>
     </div>
 
     <ul class="space-y-4">
@@ -77,29 +54,7 @@
                         {{ $answer->user->name }} | {{ $answer->created_at->diffForHumans() }}
                     </p>
 
-                    <ul class="my-4 space-y-2">
-                        @foreach ($answer->comments as $comment)
-                        <li class="flex items-center gap-2">
-                            <p class="text-xs bg-white/10 p-4 rounded-md">
-                                <span class="text-gray-500">
-                                    {{ $comment->user->name }} |
-                                    {{ $comment->created_at->diffForHumans() }}
-                                </span>
-                                <span class="text-gray-300">
-                                    {{ $comment->content }}
-                                </span>
-                            </p>
-
-                            <div>&hearts;</div>
-                        </li>
-                        @endforeach
-                    </ul>
-
-                    <p class="text-gray-500">
-                        <a href="#" class="rounded-md text-xs hover:underline cursor-pointer">
-                            Agregar comentario
-                        </a>
-                    </p>
+                    <livewire:comment :commentable="$answer" />
                 </div>
             </div>
         </li>
