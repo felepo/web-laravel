396fddfe65c445b3e50be9d1b69255eefe436972
Fernando Poncio <fecrackle@gmail.com>
2025-07-28 23:24:58 -0600

    Add AnswerController and implement answer submission functionality with form in show view

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -10,6 +10,11 @@ class Answer extends Model
     /** @use HasFactory<\Database\Factories\AnswerFactory> */
     use HasFactory;
 
+    protected $fillable = [
+        'content',
+        'user_id',
+    ];
+
     public function question()
     {
         return $this->belongsTo(Question::class);
--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,12 @@ use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\AnswerController;
 
 Route::get('/', [PageController::class, 'index'])->name('home');
 Route::get('questions/{question}', [QuestionController::class, 'show'])->name('questions.show');
+
+Route::post('answers/{question}', [AnswerController::class, 'store'])->name('answers.store');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
